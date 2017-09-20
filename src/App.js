65d7e988d92def9716308e001141bb08a064d30e
spragala <spragala@yahoo.com>
2017-09-20 14:35:34 -0700

    Added game logic

--- src/App.js
+++ src/App.js
@@ -10,41 +10,55 @@ class App extends Component {
       currentTurn: "X",
       board: [
         '','','','','','','','',''
-      ]
+      ],
+      winner: false
     }
   }
 
-handleClick(index) {
-  if(this.state.board[index] === '') {
-    this.state.board[index] = this.state.currentTurn
-    this.setState({
-      board: this.state.board,
-      currentTurn: this.state.currentTurn === this.state.PLAYER_ONE_SYMBOL ? this.state.PLAYER_TWO_SYMBOL : this.state.PLAYER_ONE_SYMBOL,
+  handleClick(index) {
+    // if (there's a winner) stop the game:
+    if (this.state.winner) {
+      alert('Press Refresh')
+    } else {
+      if(this.state.board[index] === '') {
+        this.state.board[index] = this.state.currentTurn
+        this.setState({
+          board: this.state.board,
+          currentTurn: this.state.currentTurn === this.state.PLAYER_ONE_SYMBOL
+          ? this.state.PLAYER_TWO_SYMBOL
+          : this.state.PLAYER_ONE_SYMBOL,
+        })
+      }
+      this.checkForWinner();
+    }
+  }
+
+  checkForWinner() {
+    let wins = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+    let state = this.state.board
+
+    return wins.find((combo) => {
+      if(state[combo[0]] === state[combo[1]]
+        && state[combo[0]] === state[combo[2]]
+        && state[combo[0]]) {
+        alert('Winner is ' + state[combo[0]])
+        this.setState({winner: true})
+      }
     })
+
   }
-  console.log('this.state.board: ' + this.state.board);
-  this.checkForWinner();
-}
-
-checkForWinner() {
-  let me = this
-  let wins = [[0,1,2],[3,4,5]]
-  let state = me.state.board
-
-  return wins.find(function(combo) {
-    if(state[combo[0]] === state[combo[1]] && state[combo[0]] === state[combo[2]] && state[combo[0]]) {
-      alert('Yes')
-    }
-  })
-
-}
 
   render() {
     return (
       <div className="board">
         {this.state.board.map((cell, index) => {
-          return <div onClick={this.handleClick.bind(this, index)} data-cell-id={index} className="square">{cell}</div>;
-        })}
+          return <div
+            onClick={this.handleClick.bind(this, index)}
+            data-cell-id={index}
+            className="square">{cell}
+          </div>;
+          }
+        )}
       </div>
     );
   }
