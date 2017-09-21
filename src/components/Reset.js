879941ee311913bdb3c487935c8403f9176d456b
spragala <spragala@yahoo.com>
2017-09-21 11:49:04 -0700

    WIP reset board with button in nav-bar

---
+++ src/components/Reset.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+
+
+class Reset extends Component { 
+  render() {
+     <RaisedButton label="Default" />
+  }
+}
+
+export default Reset;
