ec97fb0b7be24c883b1793e0d5560ce2584b6f72
spragala <spragala@yahoo.com>
2017-09-21 11:52:38 -0700

    Cleaned up files in src

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/index.css';
 import App from './App';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import registerServiceWorker from './registerServiceWorker';
